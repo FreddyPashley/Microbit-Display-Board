dfb39a30ee89fdb050569f6f15cf7f130bdef3c6
FreddyPashley <fredpashley1@gmail.com>
2023-12-05 11:48:02 +0000

    v0.8
    PEP8

--- client.py
+++ client.py
@@ -1,43 +1,42 @@
-VERSION = "v0.7"
-
 from microbit import *
 import radio
 import machine
 import random
 
+VERSION = "v0.8"
+
 
 class Client:
-    
+
     def __init__(self, radio_config:dict):
-        self.version = VERSION
         radio.config(**radio_config)
         radio.off()
+        self.version = VERSION
         self.serial = str(self.calculate_serial_number())
         self.location = None
 
     def calculate_serial_number(self):
         return hex(machine.mem32[268435556] & 4294967295)
     
-    def send(self, text:str, msg_id:int=None):
-        msg_id = random.randint(1, 10000) if msg_id is None else msg_id
-        # Format text with locations here
-        to_send = ["SERVER", str(msg_id), 0, text]
-        radio.send(":".join([str(i) for i in to_send]))
-    
 
-# Main
+# Main program
 client = Client(radio_config={"channel": 0, "length": 250})
 radio.on()
 
+
 while True:
     message = radio.receive()
+
     if message:  # Has a message been detected?
         message_data = message.split(":")
+
         if len(message_data) == 4:  # Is the received transmission useful to us?
             recipient, msg_id, status_code, data = message_data
+
             if recipient in [client.serial, "ALL"]:  # Is the received transmission for us?
                 msg_id = int(msg_id)
                 status_code = int(status_code)
+
                 if status_code == 3:  # Display data
                     if data.startswith("block-"):
                         brightness = data.strip("block-")
@@ -46,6 +45,7 @@ while True:
                         display.show(getattr(Image, data.upper()))
                     else:
                         display.show(data)
+
                 elif status_code == 2:  # Pre-set instruction
                     if data == "clear":
                         display.clear()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-DATA = ["Helloworld", "[happy][duck][happy][duck][happy][duck][happy][duck][happy][duck]"]
+DATA = ["Helloworld", "[happy][duck][happy][duck][happy][duck][happy][duck][happy][duck]"]  # Messages to display
 SWIPE_INTERVAL = 1.5  # Seconds for swipe to finish
 SWIPE_SHOW = .1  # Seconds for each swipe block to move
+
 """
 Valid instructions:
 clear - 'Turns off' the display
@@ -8,46 +9,48 @@ serial - All bots display their serial number for manual identification
 [image_name] - An image existing in the microbit library
 """
 
-VERSION = "v0.7"
-INSTRUCTIONS = ["clear", "serial"]
-
 from microbit import *
 import radio
 import machine
 import random
 import time
 
+VERSION = "v0.8"
+INSTRUCTIONS = ["clear", "serial"]
+
+
 class Server:
     
-    def __init__(self, radio_config:dict):
-        self.version = VERSION
+    def __init__(self, radio_config: dict):
         radio.config(**radio_config)
         radio.off()
+        self.version = VERSION
         self.serial = str(self.calculate_serial_number())
-        # self.bots = [
-        #     ["0x7b9080a4", "0x6b9a492c", "0x5cbd7385", "0x3d85ecee", "0x5415f436", "0xec2990f2", "0xd85ab8c2", "0x5a10a2c0", "0x81deabbe", "0xb1d38abc", "0x7581dc19", "0xe7c04445", "0x85fb979d", "0x6901229e", "0xbf66ffb5", "0xf7e149c0", "0xa9d48785", "0x99904c2", "0x433cea7c", "0x58058793"],
-        #     ["0x2c58cda", "0x2600e830", "0x177727a6", "0x7ad288b7", "0x964f8165", "0x1f96fdea", "0xab8b63ca", "0x9b645506", "0xb0005931", "0x2347e7f6"]
-        # ]  # Add more serials...
-        self.bots = [
-            ["0x7b9080a4", "0x6b9a492c", "0x5cbd7385", "0x3d85ecee", "0x5415f436"],
-            ["0xec2990f2", "0xd85ab8c2", "0x5a10a2c0", "0x81deabbe", "0xb1d38abc"]
-        ]  # For prototype
         self.data_pos = 0
+        """
+        self.bots = [["0x7b9080a4", "0x6b9a492c", "0x5cbd7385", "0x3d85ecee", "0x5415f436", "0xec2990f2", "0xd85ab8c2", "0x5a10a2c0", "0x81deabbe", "0xb1d38abc", "0x7581dc19", "0xe7c04445", "0x85fb979d", "0x6901229e", "0xbf66ffb5", "0xf7e149c0", "0xa9d48785", "0x99904c2", "0x433cea7c", "0x58058793"],
+                    ["0x2c58cda", "0x2600e830", "0x177727a6", "0x7ad288b7", "0x964f8165", "0x1f96fdea", "0xab8b63ca", "0x9b645506", "0xb0005931", "0x2347e7f6"]]
+        Add more serials in the future...
+        """
+        self.bots = [["0x7b9080a4", "0x6b9a492c", "0x5cbd7385", "0x3d85ecee", "0x5415f436"],
+                    ["0xec2990f2", "0xd85ab8c2", "0x5a10a2c0", "0x81deabbe", "0xb1d38abc"]]  # For prototype
 
     def calculate_serial_number(self):
         return hex(machine.mem32[268435556] & 4294967295)
     
-    def instruct(self, recipient:str="ALL", instruction:str="clear"):
+    def instruct(self, recipient: str = "ALL", instruction: str = "clear"):
         msg_id = random.randint(1, 10000)
         to_send = [recipient, str(msg_id), 2, instruction]
         radio.send(":".join([str(i) for i in to_send]))
 
-    def display(self, recipient:str="ALL", text:list=["TEST"]):
+    def display(self, recipient: str = "ALL", text: list = ["TEST"]):
         msg_id = random.randint(1, 10000)
+
         while True:
             text_list = []
             image_name = ""
             detecting_image = False
+
             for character in text[self.data_pos]:
                 if character == "[":
                     detecting_image = True
@@ -59,8 +62,10 @@ class Server:
                     image_name += character
                 else:
                     text_list.append(character)
+
             row = 0
             counter = -1
+
             for character in text_list:
                 counter += 1
                 if counter == len(self.bots[0]):
@@ -70,11 +75,16 @@ class Server:
                 recipient = self.bots[row][counter]
                 to_send = [recipient, str(msg_id), 3, character]
                 radio.send(":".join([str(i) for i in to_send]))
+
             if len(text) == 1: break
+
             time.sleep(SWIPE_INTERVAL)
             rows = len(self.bots)
             bots_per_row = len(self.bots[0])
-            brightness = {"9":[0, []],"6":[-1, []],"c":[-2, []]}
+            brightness = {"9": [0, []],
+                          "6": [-1, []],
+                          "c": [-2, []]}
+
             while brightness["9"][0] < bots_per_row:
                 brightness["9"][1] = []
                 for i in range(rows):
@@ -83,6 +93,7 @@ class Server:
                     recipient = self.bots[bot_coord[0]][bot_coord[1]]
                     to_send = [recipient, str(msg_id), 3, "block-9"]
                     radio.send(":".join([str(i) for i in to_send]))
+
                 if brightness["6"][0] >= 0:
                     brightness["6"][1] = []
                     for i in range(rows):
@@ -91,6 +102,7 @@ class Server:
                         recipient = self.bots[bot_coord[0]][bot_coord[1]]
                         to_send = [recipient, str(msg_id), 3, "block-6"]
                         radio.send(":".join([str(i) for i in to_send]))
+
                 if brightness["c"][0] >= 0:
                     brightness["c"][1] = []
                     for i in range(rows):
@@ -98,10 +110,13 @@ class Server:
                     for bot_coord in brightness["c"][1]:
                         recipient = self.bots[bot_coord[0]][bot_coord[1]]
                         self.instruct(recipient=recipient)
+
                 for k in brightness: brightness[k][0] += 1
                 time.sleep(SWIPE_SHOW)
+
             self.instruct()
-            if self.data_pos >= len(text)-1:
+
+            if self.data_pos >= len(text) - 1:
                 self.data_pos = 0
             else:
                 self.data_pos += 1
